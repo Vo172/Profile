4d848373aacf54d5787d7774cbb14d036a942bc0
VoPT <trongvo.haui@gmail.com>
2020-03-27 10:38:31 +0700

    past

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
   $(".border_btn").click(function(){
     if($("header").css("width") == "0px"){
-      $("header").css("width","300px");
+      $(window).width() > 768 ? $("header").css("width","300px") : $("header").css("width","250px");
     }
     else{
       $("header").css("width","0px");
     }
-
   });
 });
