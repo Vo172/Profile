d7f18079c37bfaecfa2346bfdcc50306892f35dc
VoPT <trongvo.haui@gmail.com>
2020-03-27 15:56:02 +0700

    add home page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $(".border_btn").click(function(){
     if($("header").css("width") == "0px"){
       $(window).width() > 768 ? $("header").css("width","300px") : $("header").css("width","250px");
+      $(window).width() > 768 ? $(".ss_home").css("margin-left","300px!important") : $("margin-left").css("width","250px!important");
     }
     else{
       $("header").css("width","0px");
