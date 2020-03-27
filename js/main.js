66824a31257a6e9413c770c050c35b58ed587e5c
VoPT <trongvo.haui@gmail.com>
2020-03-27 22:54:04 +0700

    add about

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
   $(".border_btn").click(function(){
     if($("header").css("width") == "0px"){
       $(window).width() > 768 ? $("header").css("width","300px") : $("header").css("width","250px");
-      $(window).width() > 768 ? $(".ss_home").css("margin-left","300px!important") : $("margin-left").css("width","250px!important");
+      $(window).width() > 768 ? $(".ss_home").css("padding-left","300px") : $("padding-left").css("width","250px");
+      $(".tag").addClass("humb");
     }
     else{
       $("header").css("width","0px");
+      $(".tag").removeClass("humb");
     }
-  });
+  });  
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });          
 });
