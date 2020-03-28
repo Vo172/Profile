b7592a46637d3219cf36e4d2573085eac2dfc19f
VoPT <trongvo.haui@gmail.com>
2020-03-28 12:01:14 +0700

    done about

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
-$(document).ready(function(){
-  $(".border_btn").click(function(){
-    if($("header").css("width") == "0px"){
-      $(window).width() > 768 ? $("header").css("width","300px") : $("header").css("width","250px");
-      $(window).width() > 768 ? $(".ss_home").css("padding-left","300px") : $("padding-left").css("width","250px");
+$(document).ready(function () {
+  $(".border_btn").click(function () {
+    // SET click btn menu
+    if ($("header").css("width") == "0px") {
+      $(window).width() > 768 ? $("header").css("width", "300px") : $("header").css("width", "250px");
+      $(window).width() > 768 ? $(".ss_home").css("padding-left", "300px") : $("padding-left").css("width", "250px");
       $(".tag").addClass("humb");
+
     }
-    else{
-      $("header").css("width","0px");
+    else {
+      $("header").css("width", "0px");
       $(".tag").removeClass("humb");
     }
-  });    
+  });
+
+  var body = $("body");
+  var top = body.scrollTop();
+  if (top != about) {
+    body.animate({ scrollTop: about }, '500');
+  }
+  // SET active menu 
+  $(".nav_item").click(function () {
+    $(".nav_item").removeClass("active");
+    $(this).addClass("active");
+    if ($("header").css("width") !== "0px" && $(window).width() < 1024) {
+      $("body").css("position","relative"),
+      $("header").css("width", "0px");
+      $(".tag").removeClass("humb");
+    }
+  })
+  // SET duration when click
+  $(".nav_item").click(function () {
+    let about = $(".ss_about").offset().top;
+    let home = $(".ss_home").offset().top;
+    if($(this).val() == '1'){
+      body.animate({ scrollTop: home }, '500');
+    }
+    if($(this).val() == '2'){
+      body.animate({ scrollTop: about }, '500');
+    }
+  });
+
+  // SET duration when scroll
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction(){
+    let about = $(".ss_about").offset().top;
+    let home = $(".ss_home").offset().top;
+    let top = $(".top").offset().top;
+    console.log( about + '-' + home + '-' + top)
+    if(top <= about - 100){
+      $(".nav_item").removeClass("active");
+      $(".nav_home").addClass("active");
+    }
+    else if(top >= about - 100){
+      $(".nav_item").removeClass("active");
+      $(".nav_about").addClass("active");
+    }
+  }
 });
