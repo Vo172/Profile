7d250db15164e115f66195837817b2eb40a13c9c
VoPT <trongvo.haui@gmail.com>
2020-03-29 09:46:42 +0700

    update animate

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
   // --- Preload
   $(window).on('load', function () {
-      $('#preloader').fadeOut(2000, function () {
-          $(this).remove();
-      });
+    $('#preloader').fadeOut(1000, function () {
+      $(this).remove();
+    });
   });
   // --- SET click btn menu
   $(".border_btn").click(function () {
-    
     if ($("header").css("width") == "0px") {
       $(window).width() > 768 ? $("header").css("width", "300px") : $("header").css("width", "250px");
       $(window).width() > 768 ? $(".ss_home").css("padding-left", "300px") : $("padding-left").css("width", "250px");
@@ -30,8 +29,8 @@ $(document).ready(function () {
     $(".nav_item").removeClass("active");
     $(this).addClass("active");
     if ($("header").css("width") !== "0px" && $(window).width() < 1024) {
-      $("body").css("position","relative"),
-      $("header").css("width", "0px");
+      $("body").css("position", "relative"),
+        $("header").css("width", "0px");
       $(".tag").removeClass("humb");
     }
   })
@@ -39,28 +38,74 @@ $(document).ready(function () {
   $(".nav_item").click(function () {
     let about = $(".ss_about").offset().top;
     let home = $(".ss_home").offset().top;
-    if($(this).val() == '1'){
+    if ($(this).val() == '1') {
       body.animate({ scrollTop: home }, '500');
+      addHome();
+      removeAboutLeft();
+      removeAboutRight();
     }
-    if($(this).val() == '2'){
+    if ($(this).val() == '2') {
       body.animate({ scrollTop: about }, '500');
+      removeHome();
+      addAboutLeft();
+      addAboutRight();
     }
   });
 
-  // SET duration when scroll
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction(){
+  // SET duration when scroll and animate
+  addHome();
+  window.onscroll = function () { scrollFunction() };
+  function scrollFunction() {
     let about = $(".ss_about").offset().top;
     let home = $(".ss_home").offset().top;
     let top = $(".top").offset().top;
-    console.log( about + '-' + home + '-' + top)
-    if(top <= about - 100){
-      $(".nav_item").removeClass("active");
+
+    if (top <= about - 100) {
+      $(".nav_item").removeClass("active animated flipInX");
       $(".nav_home").addClass("active");
+      $(".nav_home").addClass("active animated flipInX");
     }
-    else if(top >= about - 100){
-      $(".nav_item").removeClass("active");
-      $(".nav_about").addClass("active");
+    else if (top >= about - 100) {
+      $(".nav_item").removeClass("active animated flipInX");
+      $(".nav_about").addClass("active animated flipInX");
     }
+    if (top <= about - about / 2) {
+      addHome();
+      removeAboutLeft();
+      removeAboutRight();
+    }
+    else if (top >= about - about / 2) {
+      removeHome();
+      addAboutLeft();
+      if ($(window).width() < 768){
+        if(top >= about - about / 2 + 504)
+          addAboutRight();
+      }
+      else{
+        addAboutRight();
+      } 
+    }
+  }
+  function addHome() {
+    $(".h3").addClass("animated bounceInDown");
+    $(".p").addClass("animated delay-1s bounceInLeft");
+    $(".btn_home").addClass("animated delay-2s rotateIn");
+  }
+  function removeHome() {
+    $(".h3").removeClass("animated bounceInDown");
+    $(".p").removeClass("animated delay-1s bounceInLeft");
+    $(".btn_home").removeClass("animated delay-2s rotateIn");
+  }
+  function addAboutLeft() {
+    $(".info").addClass("animated fadeInLeft");
+  }
+  function addAboutRight() {
+    $(".txt-info").addClass("animated fadeInRight");
+  }
+  function removeAboutLeft() {
+    $(".info").removeClass("animated fadeInLeft");
+  }
+  function removeAboutRight() {
+    $(".txt-info").removeClass("animated fadeInRight");
   }
 });
